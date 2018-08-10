e14f7f6c609b61e7f89a1bdf140870b668790b04
Matthew Schmitt <matthew.schmitt@riskiq.net>
2018-08-10 15:13:01 -0700

    Increasing priority in of ci repo

--- manifests/repo/redhat.pp
+++ manifests/repo/redhat.pp
@@ -3,10 +3,11 @@ class collectd::repo::redhat {
   if $::collectd::ci_package_repo {
 
     yumrepo { 'collectd-ci':
-      ensure  => present,
-      enabled => '1',
-      baseurl => "https://pkg.ci.collectd.org/rpm/collectd-${::collectd::ci_package_repo}/epel-${::operatingsystemmajrelease}-${::architecture}",
-      gpgkey  => 'https://pkg.ci.collectd.org/pubkey.asc',
+      ensure   => present,
+      enabled  => '1',
+      priority => '1',
+      baseurl  => "https://pkg.ci.collectd.org/rpm/collectd-${::collectd::ci_package_repo}/epel-${::operatingsystemmajrelease}-${::architecture}",
+      gpgkey   => 'https://pkg.ci.collectd.org/pubkey.asc',
     }
 
   } else {
